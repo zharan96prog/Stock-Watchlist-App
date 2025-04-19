1b1b543ce5706f207254c94ac2e2dad0abc0dc08
zharan22 <o.zharan96@gmail.com>
2025-04-19 17:45:08 +0300

    Update WatchlistItem.jsx

--- src/components/WatchlistItem.jsx
+++ src/components/WatchlistItem.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 
 import Button from './UI/Button.jsx';
+import { useNavigate } from 'react-router-dom';
 
 export default function WatchlistItem({ company, onRemove }) {
   const [isRemoving, setIsRemoving] = useState(false);
+  const navigate = useNavigate();
 
   const handleRemove = async () => {
     setIsRemoving(true);
@@ -11,11 +13,26 @@ export default function WatchlistItem({ company, onRemove }) {
     setIsRemoving(false);
   };
 
+  const handleClick = () => {
+    navigate(`/watchlist/${company.symbol}`, {
+      state: { company },
+    });
+  };
+
   return (
-    <div className="flex w-full flex-row items-center justify-between p-4 border border-border rounded-lg bg-primary-dark/10 mb-4">
+    <div
+      className="flex w-full flex-row items-center justify-between p-4 border border-border rounded-lg bg-primary-dark/10 mb-4"
+      onClick={handleClick}
+    >
       <h3>{company.name}</h3>
       <p>{company.symbol}</p>
-      <Button onClick={handleRemove} disabled={isRemoving}>
+      <Button
+        onClick={(e) => {
+          e.Button.stopPropagation();
+          handleRemove();
+        }}
+        disabled={isRemoving}
+      >
         {isRemoving ? 'Removing...' : 'Remove'}
       </Button>
     </div>
