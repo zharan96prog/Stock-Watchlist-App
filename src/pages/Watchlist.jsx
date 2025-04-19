2ab26b3ce103a9945c2977b2c64455e795127ecc
zharan22 <o.zharan96@gmail.com>
2025-04-19 17:37:42 +0300

    Update Watchlist.jsx

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,21 +1,17 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import Button from '../components/UI/Button.jsx';
 import Input from '../components/UI/Input.jsx';
 import Watchlist from '../components/Watchlist.jsx';
 import { searchCompanies } from '../services/fmpService.js';
+import { useSelector } from 'react-redux';
 
 export default function WatchlistPage() {
-  const [isAddingCompany, setIsAddingCompany] = useState(false);
+  const watchlist = useSelector((state) => state.watchlist.companies);
   const [searchQuery, setSearchQuery] = useState('');
   const [companies, setCompanies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-
-  const handleAddCompany = () => {
-    setIsAddingCompany(true);
-  };
 
   const handleSearch = async (query) => {
     setSearchQuery(query);
@@ -43,42 +39,35 @@ export default function WatchlistPage() {
     <section className="mt-10 flex flex-col items-center justify-center">
       <div className="w-10/12 bg-primary-dark/10 p-4 rounded-lg border border-border text-center">
         <h2 className="text-xl font-semibold mb-4">My Watchlist</h2>
-        {!isAddingCompany ? (
+        {watchlist.length === 0 && (
           <>
-            <p className="text-sm text-primary-foreground/80">
+            <p className="text-sm mb-2 text-primary-foreground/80">
               You don't have any companies listed yet. Add the first one!
             </p>
-            <Button className="mt-5" onClick={handleAddCompany}>
-              + Add a company
-            </Button>
           </>
+        )}
+
+        <Input
+          type="text"
+          className="min-w-96"
+          placeholder="Search for a company..."
+          value={searchQuery}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+        {isLoading ? (
+          <p className="mt-4 text-sm text-primary-foreground/80">Loading...</p>
         ) : (
-          <>
-            <Input
-              type="text"
-              className="min-w-96"
-              placeholder="Search for a company..."
-              value={searchQuery}
-              onChange={(e) => handleSearch(e.target.value)}
-            />
-            {isLoading ? (
-              <p className="mt-4 text-sm text-primary-foreground/80">
-                Loading...
-              </p>
-            ) : (
-              <ul className="mt-4 text-left">
-                {companies.map((company) => (
-                  <li
-                    key={company.symbol}
-                    className="text-sm text-primary-foreground/80 cursor-pointer hover:underline"
-                    onClick={() => handleCompanyClick(company)}
-                  >
-                    {company.name} ({company.symbol})
-                  </li>
-                ))}
-              </ul>
-            )}
-          </>
+          <ul className="mt-4 text-left">
+            {companies.map((company) => (
+              <li
+                key={company.symbol}
+                className="text-sm text-primary-foreground/80 cursor-pointer hover:underline"
+                onClick={() => handleCompanyClick(company)}
+              >
+                {company.name} ({company.symbol})
+              </li>
+            ))}
+          </ul>
         )}
       </div>
       <div className="w-10/12 mt-8">
